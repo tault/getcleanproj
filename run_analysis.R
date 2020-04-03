ad9ece6eeb401ea9bb4e24cdff7c951066457db5
tault <zzyzx501@gmail.com>
2020-04-03 15:07:34 -0400

    Fixed some comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,39 @@
 ## Some comments here, but for more details see README.md and CodeBook.md
-## This script assumes you already have the required unzipped files located in a directory "uci".
-## Directory "uci" is located in another directory "data" within your working directory.
+## This script assumes you already have the required unzipped files located in your working directory.
 ## However, if you do not yet have these files or directories, you can run the code immediately below:
 ##if (!file.exists("data")){
 ##  dir.create("data")
 ##}
 ##fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-##download.file(fileURL,"./data/zippety.zip)
-##unzip(".data/zippety.zip",exdir="./data)
+##download.file(fileURL,"./data/zippety.zip")
+##unzip("./data/zippety.zip",exdir="./data")
 #file.rename("./data/UCI HAR Dataset","./data/uci")
-ftable<-read.table("./data/uci/features.txt",sep=" ")
+##setwd("./data/uci")
+##file.copy("./test/X_test.txt","./X_test.txt")
+##file.copy("./test/subject_test.txt","./subject_test.txt")
+##file.copy("./test/y_test.txt","./y_test.txt")
+##file.copy("./train/X_train.txt","./X_train.txt")
+##file.copy("./train/subject_train.txt","./subject_train.txt")
+##file.copy("./train/y_train.txt","./y_train.txt")
+ftable<-read.table("./features.txt",sep=" ")
 features<-as.character(ftable$V2)
 ## Remove upper case, periods, dashes, and random parantheses pairs from column names
 cfeatures<-gsub("\\.","",gsub("-","",gsub("\\()","",tolower(features))))
-xtest<-read.fwf("./data/uci/test/X_test.txt",rep(16,561),col.names=cfeatures)
-subtest<-read.table("./data/uci/test/subject_test.txt")
+xtest<-read.fwf("./X_test.txt",rep(16,561),col.names=cfeatures)
+subtest<-read.table("./subject_test.txt")
 subtest<-as.numeric(subtest[[1]])
 xtest$subjectid<-subtest
-ytest<-read.table("./data/uci/test/y_test.txt")
+ytest<-read.table("./y_test.txt")
 ytest<-as.numeric(ytest[[1]])
 xtest$activity<-ytest
 ## Reorder columns to have subjectid and activity first
 xtest<-xtest[c(562,563,1:561)]
 ## Repeat previous steps for training data
-xtrain<-read.fwf("./data/uci/train/X_train.txt",rep(16,561),col.names=cfeatures)
-subtrain<-read.table("./data/uci/train/subject_train.txt")
+xtrain<-read.fwf("./X_train.txt",rep(16,561),col.names=cfeatures)
+subtrain<-read.table("./subject_train.txt")
 subtrain<-as.numeric(subtrain[[1]])
 xtrain$subjectid<-subtrain
-ytrain<-read.table("./data/uci/train/y_train.txt")
+ytrain<-read.table("./y_train.txt")
 ytrain<-as.numeric(ytrain[[1]])
 xtrain$activity<-ytrain
 xtrain<-xtrain[c(562,563,1:561)]
@@ -50,4 +56,4 @@ submerged$activity<-gsub("6","lay",submerged$activity)
 library(dplyr)
 groupmerge<-group_by(submerged,subjectid,activity)
 tidy<-summarize_at(groupmerge,vars(tbodyaccmeanx:fbodybodygyrojerkmagstd),mean)
-write.table(tidy,"./data/tidy.txt",row.names=FALSE)
+write.table(tidy,"./tidy.txt",row.names=FALSE)
